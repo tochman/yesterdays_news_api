07ca962d79468ef82c4efcb7ddd2eeaf4298d8e0
DefyCab <love.bellinder@gmail.com>
2022-01-12 18:03:50 +0100

    disable generators

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,15 @@ module YesterdaysNewsApi
   class Application < Rails::Application
     config.load_defaults 6.1
     config.api_only = true
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.skip_routes true
+      generate.helper_spec false
+      generate.routing_specs false
+      generate.controller_specs false
+      generate.request_specs false
+    end
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
