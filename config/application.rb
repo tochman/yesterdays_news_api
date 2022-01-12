ad5f0a7f531b93e808425c344f6cb5c98acad5ce
DefyCab <love.bellinder@gmail.com>
2022-01-12 18:00:13 +0100

    Cleanup uneccessary comments and add middleware Rack::Cors to config/application.rb

---
+++ config/application.rb
@@ -0,0 +1,31 @@
+require_relative 'boot'
+
+require 'rails'
+
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
+
+Bundler.require(*Rails.groups)
+
+module YesterdaysNewsApi
+  class Application < Rails::Application
+    config.load_defaults 6.1
+    config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+                 headers: :any,
+                 methods: %w[get post put delete]
+      end
+    end
+  end
+end
