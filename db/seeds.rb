7e3d498747eb32d3ebe9da23b3d5e190230ec616
giacoletti <giacoletti92@gmail.com>
2022-01-20 20:48:37 +0100

    Adds articles for db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.delete_all
+
+articles = [{ 'title' => Faker::Book.title, 'body' => Faker::Quote.matz, 'category' => 'news' },
+            { 'title' => Faker::Book.title, 'body' => Faker::Quote.matz, 'category' => 'news'  },
+            { 'title' => Faker::Book.title, 'body' => Faker::Quote.matz, 'category' => 'news'  },
+            { 'title' => Faker::Book.title, 'body' => Faker::Quote.matz, 'category' => 'news'  },
+            { 'title' => Faker::Book.title, 'body' => Faker::Quote.matz, 'category' => 'news'  },
+            { 'title' => Faker::Book.title, 'body' => Faker::Quote.matz, 'category' => 'politics'  },
+            { 'title' => Faker::Book.title, 'body' => Faker::Quote.matz, 'category' => 'politics'  },
+            { 'title' => Faker::Book.title, 'body' => Faker::Quote.matz, 'category' => 'politics'  },
+            { 'title' => Faker::Book.title, 'body' => Faker::Quote.matz, 'category' => 'politics'  },
+            { 'title' => Faker::Book.title, 'body' => Faker::Quote.matz, 'category' => 'sports'  },
+            { 'title' => Faker::Book.title, 'body' => Faker::Quote.matz, 'category' => 'sports'  },
+            { 'title' => Faker::Book.title, 'body' => Faker::Quote.matz, 'category' => 'sports'  },
+            { 'title' => Faker::Book.title, 'body' => Faker::Quote.matz, 'category' => 'sports'  },
+            { 'title' => Faker::Book.title, 'body' => Faker::Quote.matz, 'category' => 'economy'  },
+            { 'title' => Faker::Book.title, 'body' => Faker::Quote.matz, 'category' => 'economy'  },
+            { 'title' => Faker::Book.title, 'body' => Faker::Quote.matz, 'category' => 'economy'  }]
+
+articles.each do |article|
+  Article.create(title: article['title'], body: article['body'], category: article['category'])
+end
