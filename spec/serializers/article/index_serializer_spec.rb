6ab53534eb3e70d736f1af457e47154762f62db0
defycab <love.bellinder@gmail.com>
2022-01-24 14:15:51 +0100

    change build_list to create_list

--- spec/serializers/article/index_serializer_spec.rb
+++ spec/serializers/article/index_serializer_spec.rb
@@ -1,12 +1,13 @@
 RSpec.describe Article::IndexSerializer, type: :serializer do
-  let(:articles) { build_list(:article, 10) }
+  let(:articles) { create_list(:article, 10) }
 
-  let(:serialization) do
+  let!(:serialization) do
     ActiveModelSerializers::SerializableResource.new(
       articles,
       each_serializer: described_class
     )
   end
+
   subject { JSON.parse(serialization.to_json) }
 
   it 'is expected to wrap content in key reflecting modal name' do
